e90e84afc16eca70da250073290d699b64e54c51
Bear-software <linner_952@mail.ru>
2024-11-15 01:21:59 +0100

    init

---
+++ src/lib/shared/data/cultureGamification.ts
@@ -0,0 +1,26 @@
+const cultureGamification = [
+	{
+		title: 'Quiz na temat kultury kawy',
+		shortDesc: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+		desc: '',
+		time: '90',
+		anchor: 'quiz',
+		tags: ['Lorem', 'Ipsum'],
+		id: '',
+		link: '',
+		img: ''
+	},
+	{
+		title: 'Warszawski Quest',
+		shortDesc: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+		desc: '',
+		tags: ['Lorem', 'Ipsum'],
+		time: '30',
+		anchor: 'quest',
+		id: '',
+		link: '',
+		img: ''
+	}
+];
+
+export default cultureGamification;
---
+++ src/lib/shared/data/supportOptions.ts
@@ -0,0 +1,22 @@
+const supportOptions = [
+	{
+		img: '',
+		title: 'Lorem Ipsum is simply dummy text',
+		id: 0,
+		amount: 10
+	},
+	{
+		img: '',
+		title: 'Lorem Ipsum is simply dummy text',
+		id: 0,
+		amount: 10
+	},
+	{
+		img: '',
+		title: 'Lorem Ipsum is simply dummy text',
+		id: 0,
+		amount: 10
+	},
+];
+
+export default supportOptions;
