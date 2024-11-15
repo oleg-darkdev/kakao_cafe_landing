b6cb87b00e2d7a21fed75d500bed4b199ff0805e
Bear-software <linner_952@mail.ru>
2024-11-15 12:29:49 +0100

    add img`s, upd values

--- src/lib/shared/data/supportOptions.ts
+++ src/lib/shared/data/supportOptions.ts
@@ -1,21 +1,21 @@
 const supportOptions = [
 	{
-		img: '',
-		title: 'Lorem Ipsum is simply dummy text',
-		id: 0,
+		img: '/images/min.png',
+		title: 'Lorem Ipsum',
+		id: 1,
 		amount: 10
 	},
 	{
-		img: '',
-		title: 'Lorem Ipsum is simply dummy text',
-		id: 0,
-		amount: 10
+		img: '/images/mid.png',
+		title: 'Lorem Ipsum',
+		id: 2,
+		amount: 20
 	},
 	{
-		img: '',
-		title: 'Lorem Ipsum is simply dummy text',
-		id: 0,
-		amount: 10
+		img: '/images/max.png',
+		title: 'Lorem Ipsum',
+		id: 3,
+		amount: 30
 	},
 ];
 
