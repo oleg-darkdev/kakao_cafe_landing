62226d3f1dcfba49c89b699b0f68fd463bff2411
Bear-software <linner_952@mail.ru>
2024-11-15 01:15:30 +0100

    init

---
+++ src/routes/[[lang=lang]]/quest/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	// import { boardgamesList, categories } from '$sharedData';
+	// import { LineDivider, SmallContaciDivider } from '$sharedUi';
+</script>
+
+<svelte:head>
+	<title>Puzzle Management | Home</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="page-wrapper">
+	<main>
+		
+	</main>
+</div>
+
+<style lang="postcss">
+							
+</style>
