923cf6d7baabb5e4c43ce6ae30a76a9fd890f6f8
Bear-software <linner_952@mail.ru>
2024-11-21 23:24:09 +0100

    refactoring data

---
+++ src/lib/shared/data/categories.ts
@@ -0,0 +1,35 @@
+const categories = [
+	{
+		title: 'Ziarenka kawy',
+		img: '/images/categories/grains.png',
+		link: '/',
+		shortDesc: ''
+	},
+	{
+		title: 'Ciepła kawa',
+		img: '/images/categories/coffee.png',
+		link: '/',
+		shortDesc: ''
+	},
+	{
+		title: 'Herbata',
+		img: '/images/categories/tea.png',
+		link: '/',
+		shortDesc: ''
+	},
+	{
+		title: 'Wypieki',
+		img: '/images/categories/cakes.png',
+		link: '/',
+		shortDesc: ''
+	},
+	{
+		title: 'Ciastka',
+		img: '/images/categories/sweets.png',
+		link: '/',
+		shortDesc: ''
+	}
+];
+
+
+export default categories;
--- src/lib/widgets/landing/ExploreMenu.svelte
+++ src/lib/widgets/landing/ExploreMenu.svelte
@@ -1,40 +1,9 @@
 <script lang="ts">
-	// import {  } from '$shared';
+	import { categories } from '$sharedData';
 	// import {  } from '$widgets';
 	// import {  } from '$entities'
 
-	const categories = [
-		{
-			title: 'Ziarenka kawy',
-			img: '/images/categories/grains.png',
-			link: '/',
-			shortDesc: ''
-		},
-		{
-			title: 'Ciepła kawa',
-			img: '/images/categories/coffee.png',
-			link: '/',
-			shortDesc: ''
-		},
-		{
-			title: 'Herbata',
-			img: '/images/categories/tea.png',
-			link: '/',
-			shortDesc: ''
-		},
-		{
-			title: 'Wypieki',
-			img: '/images/categories/cakes.png',
-			link: '/',
-			shortDesc: ''
-		},
-		{
-			title: 'Ciastka',
-			img: '/images/categories/sweets.png',
-			link: '/',
-			shortDesc: ''
-		}
-	];
+	
 </script>
 
 <section class="section_layout249">
